875d32c79e11d168fdd5db4daeb3e8d3b46bff4f
abhinay chaturvedi <abhinaychaturvedi88@gmail.com>
2024-09-18 19:58:42 +0530

    Done with Parking system design question

--- src/main/java/org/abhinay/Main.java
+++ src/main/java/org/abhinay/Main.java
@@ -1,11 +1,48 @@
 package org.abhinay;
 
+import org.abhinay.parking_lot.Level;
+import org.abhinay.parking_lot.ParkingLot;
+import org.abhinay.parking_lot.ParkingSpot;
+import org.abhinay.parking_lot.vehicle.Car;
+import org.abhinay.parking_lot.vehicle.Motorcycle;
+import org.abhinay.parking_lot.vehicle.Truck;
+import org.abhinay.parking_lot.vehicle.VehicleType;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
+    public void runParkingLotSystem(){
+        System.out.println("Running parking lot system!");
+        ParkingLot parkingLot  = ParkingLot.getInstance();
+        Level level1 = new Level(1, 4);
+//        level1.setParkingSpot(new ParkingSpot(VehicleType.CAR, 1));
+//        level1.setParkingSpot(new ParkingSpot(VehicleType.TRUCK, 2));
+//        level1.setParkingSpot(new ParkingSpot(VehicleType.MOTORCYCLE, 3));
+        Level level2 = new Level(2, 3);
+//        level2.setParkingSpot(new ParkingSpot(VehicleType.CAR, 1));
+//        level2.setParkingSpot(new ParkingSpot(VehicleType.TRUCK, 2));
+//        level2.setParkingSpot(new ParkingSpot(VehicleType.MOTORCYCLE, 3));
+        parkingLot.setLevel(level1);
+        parkingLot.setLevel(level2);
+        parkingLot.displayAvailability();
+        Car car1 = new Car("123");
+        Car car2 = new Car("124");
+        Truck truck1 = new Truck("321");
+        Truck truck2 = new Truck("421");
+        Motorcycle motor1 = new Motorcycle("motor1");
+        Motorcycle motor2 = new Motorcycle("motor2");
+        parkingLot.parkVehicle(car1);
+        parkingLot.parkVehicle(motor1);
+        parkingLot.parkVehicle(truck1);
+        parkingLot.parkVehicle(car2);
+        parkingLot.parkVehicle(truck2);
+        parkingLot.parkVehicle(motor2);
+        parkingLot.unParkVehicle(car2);
+        parkingLot.displayAvailability();
+    }
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-
+        System.out.println("hi there! This is the entry point for all the questions");
+        Main entryPoint = new Main();
+        entryPoint.runParkingLotSystem();
     }
 }
--- src/main/java/org/abhinay/parking_lot/Level.java
+++ src/main/java/org/abhinay/parking_lot/Level.java
@@ -1,6 +1,7 @@
 package org.abhinay.parking_lot;
 
 import org.abhinay.parking_lot.vehicle.Vehicle;
+import org.abhinay.parking_lot.vehicle.VehicleType;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -9,9 +10,22 @@ public class Level {
     private List<ParkingSpot> parkingSpots;
     private final int floor;
 
-    public Level(int floor) {
+    public Level(int floor, int numSpots) {
         this.floor = floor;
-        this.parkingSpots = new ArrayList<>();
+        this.parkingSpots = new ArrayList<>(numSpots);
+        // So Basically we are going to create the spots in the ration 50, 40, 10
+        // 50 for bikes, 40 cars and 10 trucks
+        int numBikes = (int) (.5 * numSpots);
+        int numCars = (int) (.4 * numSpots);
+        for(int i = 1; i <= numBikes; i++) {
+            this.parkingSpots.add(new ParkingSpot(VehicleType.MOTORCYCLE, i));
+        }
+        for(int i = numBikes + 1; i <= numBikes + numCars; i++) {
+            this.parkingSpots.add(new ParkingSpot(VehicleType.CAR, i));
+        }
+        for(int i = numBikes + numCars + 1; i <= numSpots; i++) {
+            this.parkingSpots.add(new ParkingSpot(VehicleType.TRUCK, i));
+        }
     }
 
     public List<ParkingSpot> getParkingSpots() {
