2d07bc00f6755b6629f4b4dab37ca43b3c02d6f8
abhinay chaturvedi <abhinaychaturvedi88@gmail.com>
2024-10-20 20:36:56 +0530

    starting with atm system.

---
+++ src/main/java/org/abhinay/atm_machine/Account.java
@@ -0,0 +1,32 @@
+package org.abhinay.atm_machine;
+
+public class Account {
+    private final String accountNumber;
+    private double balance;
+
+    public Account(String accountNumber) {
+        this.accountNumber = accountNumber;
+        this.balance = 0;
+    }
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public boolean withdrawMoney(double amount) {
+        if(this.balance < amount) {
+            System.out.println("Sorry! insufficient balance!");
+            return false;
+        }
+        this.balance -= amount;
+        return  true;
+    }
+    public boolean depositMoney(double amount) {
+        this.balance += amount;
+        return true;
+    }
+}
---
+++ src/main/java/org/abhinay/atm_machine/AtmMachine.java
@@ -0,0 +1,4 @@
+package org.abhinay.atm_machine;
+
+public class AtmMachine {
+}
